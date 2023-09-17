c86b10acb6ed5b4930bea158f2e8037e134f5983
UsmanMERN <mu1363131@gmail.com>
2023-09-17 15:06:20 +0500

    preloader

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Preloader from "@/components/preloader"
+export default function loading() {
+    return (
+        <>
+            <Preloader />
+        </>
+    )
+}
