ebb996032c3f5ca9dbbe8f9d2cf6817ac8fc6122
unknown <saummanzoor49@gmail.com>
2023-09-19 14:33:10 +0500

    idk

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import HeaderLg from '@/components/Header/Header'
 import Bottombar from '@/components/Header/Bottombar'
 import Canvas from '@/components/canvas'
 import Section_1 from '@/components/Home/Section_1'
+import NavigationMenu from '@/components/NavigationMenu'
+
 export default function Home() {
 
 
@@ -18,7 +20,8 @@ export default function Home() {
         <Section_1 />
         <Canvas />
 
-      </main>
-</>
+    <NavigationMenu />
+ </main>
+    </>
   )
 }
