eda3d9b45b3a8fdd5efdbe90adec097cb56d1e7e
UsmanMERN <mu1363131@gmail.com>
2023-09-19 09:54:57 +0500

    commad prompt

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 import Background from '@/components/Background'
 import ThemeSwitch from '@/lib/ThemeSwitcher'
-import {renderCanvas} from '../lib/canvaus'
+import { renderCanvas } from '../lib/canvaus'
 import { useEffect } from 'react'
 import HeaderLg from '@/components/Header/Header'
 import Bottombar from '@/components/Header/Bottombar'
@@ -10,16 +10,17 @@ import Section_1 from '@/components/Home/Section_1'
 import NavigationMenu from '@/components/NavigationMenu'
 export default function Home() {
 
-  
+
   return (
     <>
-    <Background />
-    <span className='dark:text-yellow-300'></span>
-    <Section_1 />
-    <Canvas />
+      <Background />
+      <span className='dark:text-yellow-300'></span>
+      <Section_1 />
+      <Canvas />
 
-    <NavigationMenu />
- 
+      <NavigationMenu />
+
+
     </>
   )
 }
--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -1,26 +1,101 @@
-'use client'
-import React from 'react';
+// 'use client'
+// import React from 'react';
 
-function KeyboardEventHandler() {
-  const handleKeyDown = (event:React.KeyboardEvent<HTMLDivElement>) => {
-    // Your code to handle key events goes here
-    console.log('Key pressed:', event.key);
-  };
+// function KeyboardEventHandler() {
+//   const handleKeyDown = (event:React.KeyboardEvent<HTMLDivElement>) => {
+//     // Your code to handle key events goes here
+//     console.log('Key pressed:', event.key);
+//   };
 
+//   return (
+//     <div>
+//       <p>Press a key:</p>
+//       <div
+//         onKeyDown={handleKeyDown}
+//         tabIndex={0}
+//         style={{ outline: 'none' }} // Remove the default focus outline
+//         className='bg-red-600 h-52 w-52'
+//       >
+//         {/* Content that can receive keyboard focus */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default KeyboardEventHandler;
+
+"use client"
+
+import * as React from "react"
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+
+
+export default function NavigationMenu() {
+
+  const [open, setOpen] = React.useState(false)
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
+
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  }, [])
   return (
-    <div>
-      <p>Press a key:</p>
-      <div
-        onKeyDown={handleKeyDown}
-        tabIndex={0}
-        style={{ outline: 'none' }} // Remove the default focus outline
-        className='bg-red-600 h-52 w-52'
-      >
-        {/* Content that can receive keyboard focus */}
-      </div>
-    </div>
-  );
+    <>
+      <p className="text-sm flex justify-end items-end me-11">
+        <kbd className="inline-flex h-5 select-none items-center dark:text-yellow-300 bg-black text-white gap-1 rounded font-mono text-[10px] font-medium p-5 shadow-xl border-spacing-12 cursor-pointer">
+          <span className="text-lg">⌘ J</span>
+        </kbd>
+      </p>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <span>Launch</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <span>Mail</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  )
 }
 
-export default KeyboardEventHandler;
 
