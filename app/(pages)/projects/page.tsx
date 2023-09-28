5705c5520ce53c8686341263911d5cdcb535dc84
unknown <saummanzoor49@gmail.com>
2023-09-28 14:30:20 +0500

    projects-page-height

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -8,7 +8,7 @@ import React from 'react'
 const Page = () => {
   return (
     <>
-          <div className="overflow-y-scroll scrollBar" style={{ maxHeight: '50vh' }}>
+          <div className="overflow-y-scroll scrollBar" style={{ maxHeight: '70vh' }}>
         <div className="space-y-2 pt-6 pb-8 md:space-y-5 ">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
             Projects
