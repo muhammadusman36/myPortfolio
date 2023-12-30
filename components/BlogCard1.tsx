65dd49cd3ad71acc3d084f7c5ef0fe21bfaa37be
unknown <saummanzoor49@gmail.com>
2023-12-31 00:21:28 +0500

    card-scroll-fix

--- components/BlogCard1.tsx
+++ components/BlogCard1.tsx
@@ -11,7 +11,7 @@ const BlogCard1 = ({repo}:{repo:RepoProps}) => {
     return (
         <div className='w-[280px] h-[280px] xsm:w-[340px] xsm:h-[340px] sm:w-[260px] sm:h-[260px] md:w-[330px] md:h-[330px] lg:w-[210px] lg:h-[210px] xl:w-[295px] xl:h-[295px] 2xl:w-[325px]
         2xl:h-[325px]  '>
-            <div className="card overflow-scroll scrollBar dark:shadow-[#000] ">
+            <div className="card  dark:shadow-[#000] ">
                 <a href={repo.html_url} target="_blank" rel="noopener noreferrer" className=''>
                 <div className="bg p-4 dark:bg-black/90 dark:outline-[#000] ">
                     <h6 className='text-lg'>{repo.name}</h6>
